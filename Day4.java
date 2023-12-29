d000bed695b782895d7f160566bedfed491649ab
Lavein18 <111446580+Lavein18@users.noreply.github.com>
2023-12-29 21:02:05 +0530

    Update Day4.java

--- Day4.java
+++ Day4.java
@@ -159,6 +159,118 @@ class Course {
         System.out.println("Enrolled Students in " + name + " (" + courseCode + "):");
         for (Student student : enrolledStudents) {
             System.out.println("- " + student.name + " (ID: " + student.id + ")");
+        }}}
+Quiz2 
+1)
+public class Calculator {
+   public int multiply(int num1, int num2) {
+       return num1 * num2;
+   }
+   public double multiply(double num1, double num2, double num3) {
+       return num1 * num2 * num3;
+   }
+   public static void main(String[] args) {
+       Calculator calculator = new Calculator();
+       int intResult = calculator.multiply(5, 6);
+       System.out.println("Product of integers: " + intResult);
+       double doubleResult = calculator.multiply(2.5, 3.4, 1.2);
+       System.out.println("Product of doubles: " + doubleResult);
+   }
+}
+
+2)
+class Employee {
+   String name;
+   int employeeId;
+   double basicSalary = 50000;
+   public Employee(String name, int employeeId) {
+       this.name = name;
+       this.employeeId = employeeId;
+   }
+   public double calculateSalary() {
+       return basicSalary;
+   }
+}
+class Manager extends Employee {
+   double bonusPercentage;
+   public Manager(String name, int employeeId, double bonusPercentage) {
+       super(name, employeeId);
+       this.bonusPercentage = bonusPercentage;
+   }
+   @Override
+   public double calculateSalary() {
+       double bonus = basicSalary * bonusPercentage / 100;
+       return basicSalary + bonus;
+   }
+}
+class Developer extends Employee {
+   String programmingLanguage;
+   public Developer(String name, int employeeId, String programmingLanguage) {
+       super(name, employeeId);
+       this.programmingLanguage = programmingLanguage;
+   }
+   @Override
+   public double calculateSalary() {
+       double allowance = 5000;
+       return basicSalary + allowance;
+   }
+}
+public class Main {
+   public static void main(String[] args) {
+       Manager manager1 = new Manager("John Smith", 1234, 10);
+       Developer developer1 = new Developer("Jane Doe", 5678, "Python");
+       double managerSalary = manager1.calculateSalary();
+       double developerSalary = developer1.calculateSalary();
+       System.out.println("Manager " + manager1.name + " (ID: " + manager1.employeeId + ") earns $" + managerSalary);
+       System.out.println("Developer " + developer1.name + " (ID: " + developer1.employeeId + ") earns $" + developerSalary);
+   }
+}
+
+
+3)
+public class Vehicle {
+    protected int speed;
+    public Vehicle(int speed) {
+        this.speed = speed;
+    }
+    public int calculateSpeed() {
+        return speed;
+    }
+}
+class Car extends Vehicle {
+    private int passengers;
+    public Car(int speed, int passengers) {
+        super(speed);
+        this.passengers = passengers;
+    }
+    @Override
+    public int calculateSpeed() {
+        return speed * passengers;
+    }
+}
+class Motorcycle extends Vehicle {
+    private int wheels;
+    public Motorcycle(int speed, int wheels) {
+        super(speed);
+        this.wheels = wheels;
+    }
+    @Override
+    public int calculateSpeed() {
+        return speed * wheels;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car(60, 4);
+        Motorcycle motorcycle1 = new Motorcycle(80, 2);
+        int carSpeed = car1.calculateSpeed();
+        int motorcycleSpeed = motorcycle1.calculateSpeed();
+        if (carSpeed > motorcycleSpeed) {
+            System.out.println("Car has the highest effective speed: " + carSpeed);
+        } else if (carSpeed < motorcycleSpeed) {
+            System.out.println("Motorcycle has the highest effective speed: " + motorcycleSpeed);
+        } else {
+            System.out.println("Car and motorcycle have the same effective speed: " + carSpeed);
         }
     }
 }
